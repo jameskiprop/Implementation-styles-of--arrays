2dacd495f51c2c8044c668d43418a7dfede2e9af
jameskiprop <jameskiprop01@gmail.com>
2024-06-27 21:50:50 +0300

    -update on implementation styles of arrays

---
+++ script.js
@@ -0,0 +1,79 @@
+let numArr = [12, 34, 56, 75, 79, 76];
+// for (let i = 0; i < numArr.length; i++) console.log[i];
+//Without for loop
+console.log("without the loop:");
+console.log(numArr[0]);
+console.log(numArr[1]);
+console.log(numArr[2]);
+console.log(numArr[3]);
+console.log(numArr[4]);
+console.log(numArr[5]);
+
+//FOR LOOP
+console.log("");
+console.log("Using the FOR LOOP");
+for (let i = 0; i < numArr.length; i++) {
+  console.log(numArr[i]);
+}
+// using the WHILE LOOP
+console.log("");
+console.log("using the WHILE LOOP");
+let i = 0;
+while (i < numArr.length) {
+  console.log(numArr[i]);
+  i++;
+}
+//using the DO WHILE LOOP
+console.log("");
+console.log("using the do while method");
+// let i = 0;
+do {
+  numArr[i] += 1;
+  i++; //increments each element in the numArr array by 1
+} while (i < numArr.length);
+console.log(numArr);
+
+// using a for...of method
+console.log("");
+console.log("using the for..of method");
+for (let i of numArr) {
+  console.log(numArr);
+}
+// using a forEach method
+console.log("");
+console.log("using the forEach method");
+numArr.forEach((i) => (i += 1));
+console.log(numArr);
+
+// //using a map method
+console.log("");
+console.log("using the MAP METHOD");
+let nums = "";
+let newArr = nums.map((i) => (i += 1));
+
+console.log(numArr);
+
+// //Using a reduce method
+// console.log("");
+// console.log("using the reduce method");
+
+// //Using a for....in method
+// console.log("");
+// console.log("using the for....in method");
+
+// //Using the Array.prototype.every.method
+// console.log("");
+// console.log("using the array.prototype.every method");
+
+const items = [
+  { name: "bike", price: 200 },
+  { name: "Tv", price: 100 },
+  { name: "Album", price: 10 },
+  { name: "book", price: 500 },
+  { name: "Phone", price: 1000 },
+  { name: "Computer", price: 25 },
+];
+
+const filteredItems = items.map((item) => {
+  return item.price <= 100;
+});
---
+++ tray.js
@@ -0,0 +1,9 @@
+let numArr = [23, 56, 77, 2, 98, 89];
+console.log("");
+console.log("USING THE WHILE LOOP");
+let someVar = 0;
+while (someVar < numArr.length);
+{
+  someVar++;
+}
+console.log(numArr[someVar]);
